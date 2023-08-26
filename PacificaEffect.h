0a85bc387b4fcb86aff4597ada9142477b4e669c
rin1024 <sorauta.net@gmail.com>
2023-08-26 14:37:56 +0900

    add PacificaEffect

---
+++ PacificaEffect.h
@@ -0,0 +1,74 @@
+/**
+   PacificaEffect
+*/
+#ifndef PACIFICA_EFFECT_H
+#define PACIFICA_EFFECT_H
+
+/*******************************************
+   include
+ *******************************************/
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+#include <avr/pgmspace.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <FastLED.h>
+#include "FaseLedEffect.h"
+
+/*******************************************
+   const
+ *******************************************/
+//////////////////////////////////////////////////////////////////////////
+//
+// The code for this animation is more complicated than other examples, and 
+// while it is "ready to run", and documented in general, it is probably not 
+// the best starting point for learning.  Nevertheless, it does illustrate some
+// useful techniques.
+//
+//////////////////////////////////////////////////////////////////////////
+//
+// In this animation, there are four "layers" of waves of light.  
+//
+// Each layer moves independently, and each is scaled separately.
+//
+// All four wave layers are added together on top of each other, and then 
+// another filter is applied that adds "whitecaps" of brightness where the 
+// waves line up with each other more.  Finally, another pass is taken
+// over the led array to 'deepen' (dim) the blues and greens.
+//
+// The speed and scale and motion each layer varies slowly within independent 
+// hand-chosen ranges, which is why the code has a lot of low-speed 'beatsin8' functions
+// with a lot of oddly specific numeric ranges.
+//
+// These three custom blue-green color palettes were inspired by the colors found in
+// the waters off the southern coast of California, https://goo.gl/maps/QQgd97jjHesHZVxQ7
+//
+/**************************************************************************************
+   Smoothing readings
+ **************************************************************************************/
+class PacificaEffect: public FaseLedEffect {
+  public:
+    void loop();
+
+  private:
+    CRGBPalette16 pacifica_palette_1 = 
+      { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+        0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x14554B, 0x28AA50 };
+    CRGBPalette16 pacifica_palette_2 = 
+      { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+        0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x0C5F52, 0x19BE5F };
+    CRGBPalette16 pacifica_palette_3 = 
+      { 0x000208, 0x00030E, 0x000514, 0x00061A, 0x000820, 0x000927, 0x000B2D, 0x000C33, 
+        0x000E39, 0x001040, 0x001450, 0x001860, 0x001C70, 0x002080, 0x1040BF, 0x2060FF };
+
+    void pacifica_one_layer( CRGBPalette16& p, uint16_t cistart, uint16_t wavescale, uint8_t bri, uint16_t ioff);
+    void pacifica_add_whitecaps();
+    void pacifica_deepen_colors();
+};
+
+#endif
