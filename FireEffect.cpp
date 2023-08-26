8b7eba522834e544a5a40096b9f3d0525982c808
rin1024 <sorauta.net@gmail.com>
2023-08-26 14:37:51 +0900

    add FireEffect

---
+++ FireEffect.cpp
@@ -0,0 +1,69 @@
+/**
+   FireEffect
+*/
+#include "FireEffect.h"
+
+/**
+   コンストラクタ
+*/
+FireEffect::FireEffect() {
+}
+
+/**
+   デストラクタ
+*/
+FireEffect::~FireEffect() {
+  delete[] heat;
+}
+
+/**
+ *
+ */
+void FireEffect::setup(CRGB *_leds, uint16_t _numLeds) {
+  FaseLedEffect::setup(_leds, _numLeds);
+
+  heat = new uint8_t[_numLeds];
+  for (uint16_t i=0;i<_numLeds;i++) {
+    heat[i] = 0;
+  }
+}
+
+/** 
+ * This function draws rainbows with an ever-changing,
+ * widely-varying set of parameters.
+ */
+void FireEffect::loop() {
+  // Add entropy to random number generator; we use a lot of it.
+  // random16_add_entropy( random());
+  Fire2012(); // run simulation frame
+}
+
+void FireEffect::Fire2012(){
+  // Step 1.  Cool down every cell a little
+  for( uint16_t i = 0; i < numLeds; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / numLeds) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( uint16_t k= numLeds - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if( random8() < SPARKING ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160,255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for( uint16_t j = 0; j < numLeds; j++) {
+    CRGB color = HeatColor( heat[j]);
+    int pixelnumber;
+    if( gReverseDirection ) {
+      pixelnumber = (numLeds - 1) - j;
+    } else {
+      pixelnumber = j;
+    }
+    leds[pixelnumber] = color;
+  }
+}
---
+++ FireEffect.h
@@ -0,0 +1,84 @@
+/**
+   FireEffect
+*/
+#ifndef FIRE_EFFECT_H
+#define FIRE_EFFECT_H
+
+/*******************************************
+   include
+ *******************************************/
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+#include <avr/pgmspace.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <FastLED.h>
+#include "FaseLedEffect.h"
+
+/*******************************************
+   const
+ *******************************************/
+// Fire2012 by Mark Kriegsman, July 2012
+// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
+//// 
+// This basic one-dimensional 'fire' simulation works roughly as follows:
+// There's a underlying array of 'heat' cells, that model the temperature
+// at each point along the line.  Every cycle through the simulation, 
+// four steps are performed:
+//  1) All cells cool down a little bit, losing heat to the air
+//  2) The heat from each cell drifts 'up' and diffuses a little
+//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
+//  4) The heat from each cell is rendered as a color into the leds array
+//     The heat-to-color mapping uses a black-body radiation approximation.
+//
+// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
+//
+// This simulation scales it self a bit depending on NUM_LEDS; it should look
+// "OK" on anywhere from 20 to 100 LEDs without too much tweaking. 
+//
+// I recommend running this simulation at anywhere from 30-100 frames per second,
+// meaning an interframe delay of about 10-35 milliseconds.
+//
+// Looks best on a high-density LED setup (60+ pixels/meter).
+//
+//
+// There are two main parameters you can play with to control the look and
+// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
+// in step 3 above).
+//
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100 
+#define COOLING  55
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 120
+
+/**************************************************************************************
+   Smoothing readings
+ **************************************************************************************/
+class FireEffect: public FaseLedEffect {
+  public:
+    bool gReverseDirection = true;
+
+    FireEffect();
+    ~FireEffect();
+
+    void setup(CRGB *_leds, uint16_t _numLeds = 5);
+    void loop();
+
+  private:
+    // Array of temperature readings at each simulation cell
+    uint8_t *heat;
+
+    void Fire2012();
+};
+
+#endif
