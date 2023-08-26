204ab360eef2cd132b60647d00c83b2e85802686
rin1024 <sorauta.net@gmail.com>
2023-08-26 14:37:31 +0900

    add ConfettiEffect

---
+++ ConfettiEffect.cpp
@@ -0,0 +1,37 @@
+/**
+ * ConfettiEffect
+ */
+#include "ConfettiEffect.h"
+
+/**
+ * A time (rather than loop) based demo sequencer. This gives us full control over the length of each sequence.
+ */
+void ConfettiEffect::loop() {
+  // Check the demo loop for changes to the variables
+  // Change Me
+  uint8_t secondHand = (millis() / 1000) % 15;                // IMPORTANT!!! Change '15' to a different value to change duration of the loop.
+  static uint8_t lastSecond = 99;                             // Static variable, means it's only defined once. This is our 'debounce' variable.
+  if (lastSecond != secondHand) {                             // Debounce to make sure we're not repeating an assignment.
+    lastSecond = secondHand;
+    switch(secondHand) {
+      case  0: thisinc=1; thishue=192; thissat=255; thisfade=2; huediff=256; break;  // You can change values here, one at a time , or altogether.
+      case  5: thisinc=2; thishue=128; thisfade=8; huediff=64; break;
+      case 10: thisinc=1; thishue=random16(255); thisfade=1; huediff=16; break;      // Only gets called once, and not continuously for the next several seconds. Therefore, no rainbows.
+      case 15: break;                                                                // Here's the matching 15 for the other one.
+    }
+  }
+
+  EVERY_N_MILLISECONDS(thisdelay) {                           // FastLED based non-blocking delay to update/display the sequence.
+    confetti();
+  }
+}
+
+/**
+ * random colored speckles that blink in and fade smoothly
+ */
+void ConfettiEffect::confetti() {
+  fadeToBlackBy(leds, numLeds, thisfade);                    // Low values = slower fade.
+  int pos = random16(numLeds);                               // Pick an LED at random.
+  leds[pos] += CHSV(/*(thishue + random16(huediff))/4 */0, /*thissat*/0, thisbri);  // I use 12 bits for hue so that the hue increment isn't too quick.
+  thishue = thishue + thisinc;                                // It increments here.
+}
---
+++ ConfettiEffect.h
@@ -0,0 +1,50 @@
+/**
+  ConfettiEffect
+  */
+#ifndef CONFETTI_EFFECT_H
+#define CONFETTI_EFFECT_H
+
+/*******************************************
+  include
+ *******************************************/
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+#include <avr/pgmspace.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <FastLED.h>
+#include "FaseLedEffect.h"
+
+/*******************************************
+  const
+ *******************************************/
+
+
+/**************************************************************************************
+  Smoothing readings
+ **************************************************************************************/
+class ConfettiEffect: public FaseLedEffect {
+  public:
+    void loop();
+
+  private:
+    unsigned long previousMillis;                                 // Store last time the strip was updated.
+
+    // Define variables used by the sequences.
+    uint8_t  thisfade = 8;                                        // How quickly does it fade? Lower = slower fade rate.
+    int       thishue = 50;                                       // Starting hue.
+    uint8_t   thisinc = 1;                                        // Incremental value for rotating hues
+    uint8_t   thissat = 100;                                      // The saturation, where 255 = brilliant colours.
+    uint8_t   thisbri = 255;                                      // Brightness of a sequence. Remember, max_bright is the overall limiter.
+    int       huediff = 256;                                      // Range of random #'s to use for hue
+    uint8_t thisdelay = 5;                                        // We don't need much delay (if any)
+
+    void confetti();
+};
+
+#endif
