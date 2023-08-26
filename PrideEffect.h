1e91dfe49886306f318316b5bfffa592cfd1d2c5
rin1024 <sorauta.net@gmail.com>
2023-08-26 14:38:01 +0900

    add PrideEffect

---
+++ PrideEffect.h
@@ -0,0 +1,36 @@
+/**
+   PrideEffect
+*/
+#ifndef PRIDE_EFFECT_H
+#define PRIDE_EFFECT_H
+
+/*******************************************
+   include
+ *******************************************/
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+#include <avr/pgmspace.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <FastLED.h>
+#include "FaseLedEffect.h"
+
+/*******************************************
+   const
+ *******************************************/
+//
+
+/**************************************************************************************
+   Smoothing readings
+ **************************************************************************************/
+class PrideEffect: public FaseLedEffect {
+  public:
+    void loop();
+};
+
+#endif
